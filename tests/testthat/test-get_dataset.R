87fce3e0ca096dafeb5c1c5aa98aa99d683707fc
James McMahon <james.mcmahon@phs.scot>
2024-04-10 14:33:54 +0100

    Add some new tests for `get_dataset`
    Also make tests skip if it looks like the OpenData platform isn't available.

--- tests/testthat/test-get_dataset.R
+++ tests/testthat/test-get_dataset.R
@@ -1,13 +1,45 @@
+skip_if_offline(host = "www.opendata.nhs.scot")
+
 test_that("returns data in the expected format", {
+  n_resources <- 2
+  n_rows <- 2
   data <- get_dataset(
     dataset_name = "gp-practice-populations",
-    max_resources = 2,
-    rows = 2
+    max_resources = n_resources,
+    rows = n_rows
   )
 
   expect_s3_class(data, "tbl_df")
+  expect_equal(nrow(data), n_resources * n_rows)
   expect_length(data, 24)
-  expect_equal(nrow(data), 2 * 2)
+  expect_named(data)
+})
+
+test_that("returns expected context with the data", {
+  n_resources <- 2
+  n_rows <- 2
+  data_col_names <- names(
+    get_dataset(
+      dataset_name = "gp-practice-populations",
+      max_resources = 1,
+      rows = 1
+    )
+  )
+  data <- get_dataset(
+    dataset_name = "gp-practice-populations",
+    max_resources = n_resources,
+    rows = n_rows,
+    include_context = TRUE
+  )
+
+  expect_s3_class(data, "tbl_df")
+  expect_equal(nrow(data), n_resources * n_rows)
+  expect_length(data, 28)
+  expect_named(
+    data,
+    c("res_id", "res_name", "res_created_date", "res_modified_date", data_col_names)
+  )
+  expect_length(unique(data[["res_id"]]), n_resources)
 })
 
 test_that("errors properly", {
