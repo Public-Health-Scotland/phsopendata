9a55110c315db5d6e95a72a00aa28d245684d377
James McMahon <james.mcmahon@phs.scot>
2024-04-18 16:30:29 +0100

    Run tests in parallel and split scripts to help with this
    Tests should run faster

--- tests/testthat/test-get_dataset.R
+++ tests/testthat/test-get_dataset.R
@@ -15,33 +15,6 @@ test_that("returns data in the expected format", {
   expect_named(data)
 })
 
-test_that("returns expected context with the data", {
-  n_resources <- 2
-  n_rows <- 2
-  data_col_names <- names(
-    get_dataset(
-      dataset_name = "gp-practice-populations",
-      max_resources = 1,
-      rows = 1
-    )
-  )
-  data <- get_dataset(
-    dataset_name = "gp-practice-populations",
-    max_resources = n_resources,
-    rows = n_rows,
-    include_context = TRUE
-  )
-
-  expect_s3_class(data, "tbl_df")
-  expect_equal(nrow(data), n_resources * n_rows)
-  expect_length(data, 28)
-  expect_named(
-    data,
-    c("res_id", "res_name", "res_created_date", "res_modified_date", data_col_names)
-  )
-  expect_length(unique(data[["res_id"]]), n_resources)
-})
-
 test_that("errors properly", {
   expect_error(get_dataset("Mal-formed-name"),
     regexp = "The dataset name supplied `Mal-formed-name` is invalid"
--- tests/testthat/test-get_resource.R
+++ tests/testthat/test-get_resource.R
@@ -22,100 +22,11 @@ test_that("returns data in the expected format", {
   expect_equal(data_q[["PracticeCode"]], 10002)
 })
 
-test_that("returns expected context with the data", {
-  gp_list_apr_2021 <- "a794d603-95ab-4309-8c92-b48970478c14"
-  data_col_names <- names(get_resource(res_id = gp_list_apr_2021, rows = 1))
-
-  # without query
-  data <- get_resource(
-    res_id = gp_list_apr_2021,
-    rows = 1,
-    include_context = TRUE
-  )
-
-  expect_s3_class(data, "tbl_df")
-  expect_length(data, 19)
-  expect_equal(nrow(data), 1)
-  expect_named(data, c(
-    "res_id",
-    "res_name",
-    "res_created_date",
-    "res_modified_date",
-    data_col_names
-  ))
-
-  # with query
-  data_q <- get_resource(
-    gp_list_apr_2021,
-    row_filters = list(PracticeCode = 10002),
-    col_select = c("PracticeCode", "AddressLine1"),
-    include_context = TRUE
-  )
-
-  expect_named(data_q, c(
-    "res_id",
-    "res_name",
-    "res_created_date",
-    "res_modified_date",
-    "PracticeCode",
-    "AddressLine1"
-  ))
-  expect_equal(data_q[["PracticeCode"]], 10002)
-})
-
-test_that("checks res_id properly", {
-  # wrong type
-  expect_error(
-    get_resource(res_id = 123),
-    regexp = "(must be of type character)"
-  )
-  # Invalid format (doesn't match regex)
-  expect_error(
-    get_resource("a794d603-95ab-4309-8c92-b48970478c1"),
-    regexp = "(is in an invalid format.)"
-  )
-  # res_id is a vector of length > 1
-  expect_error(
-    get_resource(1:5),
-    regexp = "(must be of length 1.)"
-  )
-  # Correct format but not real
-  expect_error(
-    get_resource("00000000-0000-0000-0000-000000000000"),
-    regexp = "(Can't find resource with ID)"
-  )
-})
-
-test_that("returns full data if only res_id is input", {
+test_that("returns data with row specifications", {
   gp_list_apr_2021 <- "a794d603-95ab-4309-8c92-b48970478c14"
 
-  data <- get_resource(res_id = gp_list_apr_2021)
+  expect_equal(nrow(get_resource(res_id = gp_list_apr_2021, rows = 926)), 926)
 
-  expect_equal(nrow(data), 926)
-})
-
-test_that("returns full data if rows is set to over 99999", {
-  gp_list_apr_2021 <- "a794d603-95ab-4309-8c92-b48970478c14"
-
-  expect_warning(
-    data <- get_resource(
-      res_id = gp_list_apr_2021,
-      rows = 9999999,
-      row_filters = c("GPPracticeName" = "The Blue Practice")
-    ),
-    regexp = "Can't request over 99,999 rows"
-  )
-
-  expect_equal(nrow(data), 926)
-})
-
-test_that("first 99999 rows returned if query matches > 99999 rows", {
-  prescriptions_apr_2021 <- "51b7ad3f-6d52-4165-94f4-92e322656c85"
-
-  expect_warning(
-    df <- get_resource(prescriptions_apr_2021, col_select = c("HBT")),
-    regexp = "(Returning the first 99999 results)"
-  )
-
-  expect_true(nrow(df) == 99999)
+  expect_equal(nrow(get_resource(res_id = gp_list_apr_2021, rows = 999)), 926) %>%
+    expect_warning()
 })
