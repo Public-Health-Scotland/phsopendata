961cc951bcb901c8f7f2da731fd708cc9185fe2b
Moohan <Moohan@users.noreply.github.com>
2024-04-18 16:45:05 +0000

    Style code (GHA)

--- tests/testthat/test-error_check.R
+++ tests/testthat/test-error_check.R
@@ -3,7 +3,7 @@ skip_if_offline(host = "www.opendata.nhs.scot")
 test_that("returns nothing if no error", {
   expect_null(
     error_check(phs_GET("package_list", ""))
-    )
+  )
 })
 
 test_that("throws error if error in httr content", {
