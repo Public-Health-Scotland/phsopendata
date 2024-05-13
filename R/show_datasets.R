b94c68c3caf31fd3fb80571714b9916e5b134406
ross hull <ross.hull2@phs.scot>
2024-05-13 15:18:23 +0100

    add roxygen comment to R/show_datasets.R

--- R/show_datasets.R
+++ R/show_datasets.R
@@ -1,5 +1,16 @@
+#' Show all available datasets
+#'
+#' `show_datasets()`Shows all datasets available form phs open data platform.
+#'
+#' @return A character vector.
+#' @export
+#'
+#' @examples
+#' show_datasets()
+#'
 show_datasets <- function() {
-  data_sets <- phs_GET("package_list", "")$result
+  data_sets <- phs_GET("package_list", "")$result %>%
+    unlist()
 
   return(data_sets)
 }
