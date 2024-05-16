926ba598f49c774ca1339b49d5cc159d1bf0e81f
ross hull <ross.hull2@phs.scot>
2024-05-16 14:01:38 +0100

    write get_dataset_additional_info function

--- R/get_dataset_additional_info.R
+++ R/get_dataset_additional_info.R
@@ -1,4 +1,25 @@
 get_datasets_additional_info <- function(dataset_name){
+  query <- paste0("id=", dataset_name)
+  content <- phs_GET("package_show", query)
+
+  amount_of_resources <- content$result$resources %>%
+    length()
+
+  last_resource_created_date <- purrr::map(content$result$resources, ~.$created) %>%
+    unlist() %>%
+    max()
+
+  last_resource_modified_date <- purrr::map(content$result$resources, ~.$last_modified) %>%
+    unlist() %>%
+    max()
+
+  most_recent_resource_date <- c(last_resource_modified_date, last_resource_created_date) %>%
+    max()
+
+  return_value <- list(amount_of_resources, most_recent_resource_date)
+
+  return(return_value)
 
 }
+
 
