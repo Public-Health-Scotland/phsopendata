0664388260dcd7e04e4a3d692e9edd72a17eaee1
ross-hull <117927497+ross-hull@users.noreply.github.com>
2024-05-22 12:21:15 +0100

    Update method of max date retrieval R/get_dataset_additional_info.R
    Co-authored-by: James McMahon <james.mcmahon@phs.scot>

--- R/get_dataset_additional_info.R
+++ R/get_dataset_additional_info.R
@@ -11,16 +11,11 @@ get_datasets_additional_info <- function(dataset_name){
   amount_of_resources <- content$result$resources %>%
     length()
 
-  last_resource_created_date <- purrr::map(content$result$resources, ~.$created) %>%
-    unlist() %>%
-    max()
+  last_resource_created_date <- purrr::map_chr(content$result$resources, ~.$created)
 
-  last_resource_modified_date <- purrr::map(content$result$resources, ~.$last_modified) %>%
-    unlist() %>%
-    max()
+  last_resource_modified_date <- purrr::map_chr(content$result$resources, ~.$last_modified)
 
-  most_recent_resource_date <- c(last_resource_modified_date, last_resource_created_date) %>%
-    max()
+  most_recent_resource_date <- max(last_resource_modified_date, last_resource_created_date)
 
   return_value <- list("name" = dataset_name,
                          "amount_of_resources" = amount_of_resources,
