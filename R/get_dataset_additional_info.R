d8cb21ef7a2c955d92b85f5f29f9fba45c95398c
ross-hull <ross-hull@users.noreply.github.com>
2024-06-14 15:20:11 +0000

    Style code (GHA)

--- R/get_dataset_additional_info.R
+++ R/get_dataset_additional_info.R
@@ -4,25 +4,24 @@
 #' @param dataset_name
 #'
 #' @return
-get_datasets_additional_info <- function(dataset_name){
+get_datasets_additional_info <- function(dataset_name) {
   query <- list("id" = dataset_name)
   content <- phs_GET("package_show", query)
 
   amount_of_resources <- content$result$resources %>%
     length()
 
-  last_resource_created_date <- purrr::map_chr(content$result$resources, ~.$created)
+  last_resource_created_date <- purrr::map_chr(content$result$resources, ~ .$created)
 
-  last_resource_modified_date <- purrr::map_chr(content$result$resources, ~.$last_modified)
+  last_resource_modified_date <- purrr::map_chr(content$result$resources, ~ .$last_modified)
 
   most_recent_resource_date <- max(last_resource_modified_date, last_resource_created_date)
 
-  return_value <- list("name" = dataset_name,
-                         "amount_of_resources" = amount_of_resources,
-                         "most_recent_resource_date" = most_recent_resource_date)
+  return_value <- list(
+    "name" = dataset_name,
+    "amount_of_resources" = amount_of_resources,
+    "most_recent_resource_date" = most_recent_resource_date
+  )
 
   return(return_value)
-
 }
-
-
--- R/list_datasets.R
+++ R/list_datasets.R
@@ -13,25 +13,21 @@
 #' head(list_datasets(include_additional_info = TRUE))
 list_datasets <- function(include_additional_info = FALSE) {
   data_sets <- phs_GET("package_list", "")$result
-  if(!include_additional_info){
+  if (!include_additional_info) {
     data_sets <- tibble::tibble("name" = unlist(data_sets))
 
 
-  return(data_sets)}
-  else{
+    return(data_sets)
+  } else {
     datasets <- purrr::map(data_sets, get_datasets_additional_info)
 
-    return_value <- tibble::tibble(name = purrr::map_chr(datasets, ~.$name),
-                                   most_recent_resource_date = purrr::map_chr(datasets, ~.$most_recent_resource_date),
-                                   amount_of_resources = purrr::map_int(datasets, ~.$amount_of_resources)) %>%
+    return_value <- tibble::tibble(
+      name = purrr::map_chr(datasets, ~ .$name),
+      most_recent_resource_date = purrr::map_chr(datasets, ~ .$most_recent_resource_date),
+      amount_of_resources = purrr::map_int(datasets, ~ .$amount_of_resources)
+    ) %>%
       dplyr::mutate(most_recent_resource_date = as.POSIXct(most_recent_resource_date, format = "%FT%X", tz = "UTC"))
 
     return(return_value)
   }
 }
-
-
-
-
-
-
