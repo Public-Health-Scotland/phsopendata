db691aa6dce2422760b417b1d659bb534e73691f
ross hull <ross.hull2@phs.scot>
2024-06-20 15:29:36 +0100

    refine get_dataset_additional_info

--- R/get_dataset_additional_info.R
+++ R/get_dataset_additional_info.R
@@ -4,6 +4,7 @@
 #' @param dataset_name
 #'
 #' @return
+#' @export
 get_datasets_additional_info <- function(dataset_name){
   query <- list("id" = dataset_name)
   content <- phs_GET("package_show", query)
@@ -15,11 +16,12 @@ get_datasets_additional_info <- function(dataset_name){
 
   last_resource_modified_date <- purrr::map_chr(content$result$resources, ~.$last_modified)
 
-  most_recent_resource_date <- max(last_resource_modified_date, last_resource_created_date)
+  most_recent_resource_date <- max(last_resource_modified_date, last_resource_created_date) %>%
+    as.POSIXct(format = "%FT%X", tz = "UTC")
 
-  return_value <- list("name" = dataset_name,
+  return_value <- tibble::tibble("name" = dataset_name,
                          "amount_of_resources" = amount_of_resources,
-                         "most_recent_resource_date" = most_recent_resource_date)
+                         "most_recent_resource_update" = most_recent_resource_date)
 
   return(return_value)
 
