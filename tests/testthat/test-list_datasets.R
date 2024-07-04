b5edfac323b56ae7df5b50c3ea7c6d133079fbf2
James McMahon <james.mcmahon@phs.scot>
2024-07-04 12:00:00 +0100

    Update tests/testthat/test-list_datasets.R

--- tests/testthat/test-list_datasets.R
+++ tests/testthat/test-list_datasets.R
@@ -2,7 +2,7 @@ test_that("returns more than 0 datasets", {
   # select the first row of the tibble and get the
   # number of rows. If no datasets were returned
   # this will be 0
-  expect_equal(nrow(slice(list_datasets(), 1)), 1)
+  expect_equal(nrow(dplyr::slice(list_datasets(), 1)), 1)
 })
 
 test_that("returns data in the expected format", {
