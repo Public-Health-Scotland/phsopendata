9b01c7530dde9a92c3f6e3712638adab6b5c4a48
ross hull <ross.hull2@phs.scot>
2024-07-11 14:06:07 +0100

    update test

--- tests/testthat/test-get_latest_resource.R
+++ tests/testthat/test-get_latest_resource.R
@@ -2,9 +2,9 @@ test_that("returns more than 0 datasets", {
   # select the first row of the tibble and get the
   # number of rows. If no datasets were returned
   # this will be 0
-  expect_equal(nrow(dplyr::slice(list_datasets(), 1)), 1)
+  expect_equal(nrow(dplyr::slice(get_latest_resource(), 1)), 1)
 })
 
 test_that("returns data in the expected format", {
-  expect_s3_class(list_datasets(), "tbl_df")
+  expect_s3_class(get_latest_resource(), "tbl_df")
 })
