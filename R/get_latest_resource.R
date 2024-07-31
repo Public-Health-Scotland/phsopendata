1ea8c6ed50d0710749677f86cf36458c43514fac
ross hull <ross.hull2@phs.scot>
2024-07-31 10:23:07 +0100

    remove unnecisary dataset name checks get_latest_resource

--- R/get_latest_resource.R
+++ R/get_latest_resource.R
@@ -57,21 +57,8 @@ get_latest_resource <- function(dataset_name,
     "prescriptions-in-the-community", "community-pharmacy-contractor-activity"
   )
 
-  # throw error if name type/format is invalid
-  check_dataset_name(dataset_name)
 
-  # define query and try API call
-  query <- list("id" = dataset_name)
-  content <- try(
-    phs_GET("package_show", query),
-    silent = TRUE
-  )
 
-  # if content contains a 'Not Found Error'
-  # throw error with suggested dataset name
-  if (grepl("Not Found Error", content[1])) {
-    suggest_dataset_name(dataset_name)
-  }
 
   # check if data set is within applicable datasets
   # throw error if not
