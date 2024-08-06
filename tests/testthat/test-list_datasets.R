295b558af766ac358428ed178ac014c37ce22d9a
James McMahon <james.mcmahon@phs.scot>
2024-08-06 15:46:34 +0100

    Simplify test

--- tests/testthat/test-list_datasets.R
+++ tests/testthat/test-list_datasets.R
@@ -1,10 +1,7 @@
 skip_if_offline(host = "www.opendata.nhs.scot")
 
 test_that("returns more than 0 datasets", {
-  # select the first row of the tibble and get the
-  # number of rows. If no datasets were returned
-  # this will be 0
-  expect_equal(nrow(dplyr::slice(list_datasets(), 1)), 1)
+  expect_gte(nrow(list_datasets()), 1)
 })
 
 test_that("returns data in the expected format", {
