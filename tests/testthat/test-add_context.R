0e7e056b0354748edc8f89a0f911282608691c3c
James McMahon <james.mcmahon@phs.scot>
2025-01-21 15:57:39 +0000

    Improve test coverage
    Added some tests to improve the coverage

--- tests/testthat/test-add_context.R
+++ tests/testthat/test-add_context.R
@@ -24,6 +24,7 @@ test_that("Returned context is the same for resource and dataset", {
       dplyr::filter(ResID == res_id_1) %>%
       dplyr::select(!dplyr::where(~ anyNA(.x))),
     resource_1,
+    # list_as_map = TRUE will sort variable names before comparing
     list_as_map = TRUE
   )
   expect_equal(
@@ -33,5 +34,45 @@ test_that("Returned context is the same for resource and dataset", {
     resource_2,
     list_as_map = TRUE
   )
-  # list_as_map = TRUE will sort variable names before comparing
+})
+
+test_that("add_context works with odd data", {
+  res_id <- "647b256e-4a03-4963-8402-bf559c9e2fff"
+
+  data <- get_resource(
+    res_id = res_id,
+    rows = 5
+  ) %>%
+    add_context(
+      id = res_id,
+      name = "GP Details April 2024",
+      created_date = "2025-01-01T00:00:00",
+      modified_date = NULL
+    )
+
+  expect_s3_class(data, "tbl_df")
+  expect_named(data, c(
+    "ResID",
+    "ResName",
+    "ResCreatedDate",
+    "ResModifiedDate",
+    "GeneralMedicalCouncilNumber",
+    "GPDesignation",
+    "Forename",
+    "MiddleInitial",
+    "Surname",
+    "Sex",
+    "SexQF",
+    "PracticeCode",
+    "GPPracticeName",
+    "AddressLine1",
+    "AddressLine2",
+    "AddressLine3",
+    "AddressLine4",
+    "Postcode",
+    "Telephone",
+    "HB",
+    "HSCP"
+  ))
+  expect_true(all(is.na(data$ResModifiedDate)))
 })
