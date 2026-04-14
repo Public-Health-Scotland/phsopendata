ceccdd2c1b9d284d74d10ea09155ffa2fc323a57
James Hayes (né McMahon) <james.hayes2@phs.scot>
2026-04-14 11:04:57 +0100

    Delete tests/testthat/test-list_all_resources.R

--- tests/testthat/test-list_all_resources.R
+++
@@ -1,7 +0,0 @@
-test_that("list_all_resources() is deprecated with a message pointing to list_resources()", {
-  skip_if_offline(host = "www.opendata.nhs.scot")
-  expect_warning(
-    list_all_resources(),
-    class = "lifecycle_warning_deprecated"
-  )
-})
