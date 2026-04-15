f0aaedab1e019c3e282c9bee14c7cfa443d2c491
James Hayes <james.hayes2@phs.scot>
2026-04-15 09:11:06 +0000

    Move and rename `list_resources_query`

--- R/list_resources.R
+++ R/list_resources.R
@@ -114,7 +114,7 @@ list_resources <- function(
   validate_regex(resource_contains, "resource_contains")
   validate_regex(dataset_contains, "dataset_contains")
 
-  data_tibble <- list_all_resources_query()
+  data_tibble <- list_resources_query()
 
   if (!is.null(resource_contains)) {
     data_tibble <- data_tibble[
@@ -137,6 +137,54 @@ list_resources <- function(
       )
     }
   }
+
+  return(data_tibble)
+}
+
+
+list_resources_query <- function() {
+  # query for the API call
+  query <- list(q = "*:*", rows = 3200)
+
+  # API call
+  content <- phs_GET(action = "package_search", query = query)
+
+  # extract the data
+  results <- purrr::chuck(content, "result", "results")
+
+  datasets_list <- purrr::map(
+    results,
+    function(dataset) {
+      resources <- purrr::chuck(dataset, "resources")
+
+      tibble::tibble(
+        resource_name = purrr::map_chr(
+          resources,
+          ~ purrr::pluck(.x, "name", .default = NA_character_)
+        ),
+        resource_id = purrr::map_chr(
+          resources,
+          ~ purrr::pluck(.x, "id", .default = NA_character_)
+        ),
+        dataset_name = purrr::chuck(dataset, "name"),
+        dataset_id = purrr::chuck(dataset, "id"),
+        url = purrr::map_chr(
+          resources,
+          ~ purrr::pluck(.x, "url", .default = NA_character_)
+        ),
+        last_modified = as.POSIXct(
+          purrr::map_chr(
+            resources,
+            ~ purrr::pluck(.x, "last_modified", .default = NA_character_)
+          ),
+          format = "%FT%X",
+          tz = "UTC"
+        )
+      )
+    }
+  )
+
+  data_tibble <- purrr::list_rbind(datasets_list)
 
   return(data_tibble)
 }
